8a4cce17aa5f31329e4128704b44ab2612e48340
Yahia <yahyaaymanqotp@gmail.com>
2026-09-05 13:39:40 +0300

    Fix OneSignal Service Worker

--- src/main.jsx
+++ src/main.jsx
@@ -1,3 +1,6 @@
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('/sw.js');
+}
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
